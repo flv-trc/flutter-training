1d0e717c6821b5238e53487cdf1020391613cce0
Flavius Turcu <flaviusturcu@coherentsolutions.com>
2025-05-30 10:18:29 +0300

    part-2-auth: remove validation logic, will come in the next PR; removed redundant User

--- lib/features/login/login_viewmodel.dart
+++ lib/features/login/login_viewmodel.dart
@@ -5,17 +5,10 @@ class LoginViewModel with ChangeNotifier {
   final passwordController = TextEditingController();
 
   String? validateEmail(String? value) {
-    if (value == null ||
-        value.isEmpty ||
-        value.length > 255 ||
-        !value.contains('@')) {
-      return 'Please enter a valid email address.';
-    }
     return null;
   }
 
   String? validatePassword(String? value) {
-    if (value == null || value.length < 6) return 'Min 6 chars';
     return null;
   }
 
