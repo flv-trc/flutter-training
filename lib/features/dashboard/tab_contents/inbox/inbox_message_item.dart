8202cfcbe73abdf394b2f9a901aa0678eaefe5ff
Flavius Turcu <flaviusturcu@coherentsolutions.com>
2025-06-13 14:24:13 +0300

    part-4: lesson 2

--- lib/features/dashboard/tab_contents/inbox/inbox_message_item.dart
+++ lib/features/dashboard/tab_contents/inbox/inbox_message_item.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_training/features/webview/webview_viewmodel.dart';
 
 import '../../../../routing/exports.dart';
 import 'inbox_message.dart';
